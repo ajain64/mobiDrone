2fe9f00f91a025e8d75aa8c6e0d73a856f8b95f8
ajain64 <ajain64@asu.edu>
2017-04-20 23:09:21 -0700

    Update SettingsActivity.java

--- Project/app/src/main/java/asu/cse535/group3/project/SettingsActivity.java
+++ Project/app/src/main/java/asu/cse535/group3/project/SettingsActivity.java
@@ -13,6 +13,17 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
+        Button fButton = (Button) findViewById(R.id.settingsFeedback);
+        fButton.setOnClickListener( new android.view.View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SettingsActivity.this, MessageCenter.class);
+                startActivity(intent);
+            }
+
+        });
+
         Button backButton = (Button) findViewById(R.id.backButton);
         backButton.setOnClickListener( new android.view.View.OnClickListener() {
 
